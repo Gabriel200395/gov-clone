7d28d05975645ddac41520e7be8a00118c05dc3d
Gabriel Souza <eugabrielf@gmail.com>
2021-02-26 12:52:53 -0300

    mudando fonts

--- src/Component/ElementsForm/Titulo.js
+++ src/Component/ElementsForm/Titulo.js
@@ -2,7 +2,7 @@ function Titulo(props) {
   const { titulo } = props;
   return (
     <div className="content-titulo">
-      <h2>{titulo}</h2>
+      <h4>{titulo}</h4>
       <i id="icon-question" className="fas fa-question"></i>
     </div>
   );
